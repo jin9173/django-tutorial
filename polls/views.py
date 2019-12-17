cb22e2e0b81523a3e07c688424f29af53e7cce2d
jin9173 <jinhappier2@gmail.com>
2019-12-17 17:19:17 +0900

    detail, results, vote view와 url연결

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(request, question_id):
+    response = "You're looking at the results of question {}"
+    return HttpResponse(response.format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
+
